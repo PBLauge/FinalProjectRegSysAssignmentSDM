e2979f48813213bfe8acd8b161f08da7395b5426
PBLauge <peder.laugesen@gmail.com>
2017-09-19 11:38:38 +0200

    first commit

---
+++ ProjectRegistration/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProjectRegistration
+{
+    public class Class1
+    {
+    }
+}
---
+++ ProjectRegitrationTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectRegitrationTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjectRegitrationTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b5c4268d-0395-4043-a500-1adc02a5efe7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
